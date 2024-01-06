e0e7cf9d4c36c4ac63dcef8e53118181cf828229
pierluigi <pierluigicassotti@gmail.com>
2024-01-06 02:56:53 +0100

    update folder

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
     long_description="",
     long_description_content_type="text/markdown",
     url="https://github.com/pierluigic/LSCDetection",
-    packages=['LSCDetection'],
+    packages=setuptools.find_packages(),
     platforms=['all'],
     classifiers=[
         "Programming Language :: Python :: 3",
